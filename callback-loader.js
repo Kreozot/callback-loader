26b353936bdadf4fb420bdc083fb375ad33f6e35
s.sharov <s.sharov@zebra-group.ru>
2015-10-06 19:19:41 +0300

    Working version

--- callback-loader.js
+++ callback-loader.js
@@ -1,15 +1,43 @@
-var acorn = require("acorn");
-var fs = require('fs');
+var astQuery = require("ast-query");
+var loaderUtils = require("loader-utils");
 
-var source = fs.readFileSync('./test.js');
+module.exports = function (source) {
+	var query = loaderUtils.parseQuery(this.query);
 
-var indexOffset = 0; //Offset for future replaces
-function replaceIn(text, indexFrom, indexTo, replaceText) {
-	actualIndexFrom = indexFrom + indexOffset;
-	actualIndexTo = indexTo + indexOffset;
-	indexOffset = replaceText.length - (indexTo - indexFrom); //Correcting the offset
-	return text.substr(0, actualIndexFrom) + replaceText + text.substr(actualIndexTo, text.length);
+	var functions = this.options;
+	var functionNames = Object.keys(query);
+	if (functionNames.length === 0) {
+		functionNames = Object.keys(functions);
+	}
+
+	//Offset for future replaces
+	var indexOffset = 0;
+
+	//Replace substring between *indexFrom* and *indexTo* in *text* with *replaceText*
+	function replaceIn(text, indexFrom, indexTo, replaceText) {
+		var actualIndexFrom = indexFrom + indexOffset;
+		var actualIndexTo = indexTo + indexOffset;
+		//Correcting the offset
+		indexOffset = indexOffset + replaceText.length - (indexTo - indexFrom);
+		return text.substr(0, actualIndexFrom) + replaceText + text.substr(actualIndexTo, text.length);
+	}
+
+	var ast = astQuery(source);
+
+	functionNames.forEach(function (funcName) {
+		var query = ast.callExpression(funcName);
+
+		query.nodes.forEach(function (node) {
+			var args = node.arguments.map(function (argument) {
+				if (argument.type !== 'Literal') {
+					throw 'Error when parsing arguments of function ' + funcName + '. Only absolute values accepted. Index: ' + argument.range[0];
+				}
+				return argument.value;
+			});
+			var value = functions[funcName].apply(null, args);
+			source = replaceIn(source, node.range[0], node.range[1], value.toString());
+		});
+	});
+
+	return source;
 }
-
-var ast = acorn.parse(source);
-console.log(JSON.stringify(ast, null, ' '));
