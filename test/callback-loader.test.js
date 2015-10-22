ecc3f1bd99b7df74144057c3cf7a4011e9d97640
Kreozot <zzxyyz@gmail.com>
2015-10-22 13:00:02 +0300

    Tests refactored.

--- test/callback-loader.test.js
+++ test/callback-loader.test.js
@@ -2,6 +2,8 @@ var should = require("should");
 var path = require("path");
 var loader = require("../");
 
+loader.cacheable = function() {};
+
 describe("loader", function() {
 
 	var options = {
@@ -32,81 +34,70 @@ describe("loader", function() {
 		}
 	}
 
+	var context = {
+		cacheable: function() {},
+		options: options
+	};
+
 	it("should process all functions without query", function() {
-		loader.call({
-				options: options,
-				query: ''
-			}, 'var a = multBy2(10); var b = mult(10, 3);')
+		context.query = '';
+		loader.call(context, 'var a = multBy2(10); var b = mult(10, 3);')
 			.should.be.eql(
 				'var a = 20; var b = 30;'
 			);
 	});
 
 	it("should process complicated structure", function() {
-		loader.call({
-				options: options,
-				query: ''
-			}, 'var a = multBy2(10); var b = mult(10, 3); var c = multBy2(12); var d = mult(12, 3);')
+		context.query = '';
+		loader.call(context, 'var a = multBy2(10); var b = mult(10, 3); var c = multBy2(12); var d = mult(12, 3);')
 			.should.be.eql(
 				'var a = 20; var b = 30; var c = 24; var d = 36;'
 			);
 	});
 
 	it("should process one function with one parameter", function() {
-		loader.call({
-				options: options,
-				query: '?multBy2'
-			}, 'var a = multBy2(10); var b = mult(10, 3);')
+		context.query = '?multBy2';
+		loader.call(context, 'var a = multBy2(10); var b = mult(10, 3);')
 			.should.be.eql(
 				'var a = 20; var b = mult(10, 3);'
 			);
 	});
 
 	it("should process multiple function with multiple parameters", function() {
-		loader.call({
-				options: options,
-				query: '?multBy2&mult'
-			}, 'var a = multBy2(10); var b = mult(10, 3);')
+		context.query = '?multBy2&mult';
+		loader.call(context, 'var a = multBy2(10); var b = mult(10, 3);')
 			.should.be.eql(
 				'var a = 20; var b = 30;'
 			);
 	});
 
 	it("should process function with float args", function() {
-		loader.call({
-				options: options,
-				query: '?multBy2'
-			}, 'var a = multBy2(1.2);')
+		context.query = '?multBy2';
+		loader.call(context, 'var a = multBy2(1.2);')
 			.should.be.eql(
 				'var a = 2.4;'
 			);
 	});
 
 	it("should process function with string args", function() {
-		loader.call({
-				options: options,
-				query: ''
-			}, 'var a = concat("foo", "bar");')
+		context.query = '';
+		loader.call(context, 'var a = concat("foo", "bar");')
 			.should.be.eql(
 				'var a = "foobar";'
 			);
 	});
 
 	it("should take another config", function() {
-		loader.call({
-				options: options,
-				query: '?config=callbackLoader2'
-			}, 'var a = concat("foo", "bar");')
+		context.query = '?config=callbackLoader2';
+		loader.call(context, 'var a = concat("foo", "bar");')
 			.should.be.eql(
 				'var a = "foobar-version2";'
 			);
 	});
 
 	it("should process function with object args", function() {
-		loader.call({
-				options: options,
-				query: ''
-			}, 'var a = getSecond({first: 1, second: 2});')
+		context.query = '';
+		loader.call(context, 'var a = getSecond({first: 1, second: 2});')
 			.should.be.eql(
 				'var a = 2;'
 			);
